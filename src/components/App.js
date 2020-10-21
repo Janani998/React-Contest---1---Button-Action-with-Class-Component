051f8f333b8442b9fa3decff7b069cab8e7efcd8
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-10-21 20:55:03 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import Para from "./Para";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
-
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+  }
+  handleClick() {
+    this.setState({ show: true });
+  }
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick={() => this.handleClick}>
+          Click
+        </button>
+        {this.state.show ? <Para /> : null}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
